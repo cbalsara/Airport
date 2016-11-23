52a1363c80969a925520af066a369e95d52e8ae6
Cyrus <cdbalsara@icloud.com>
2016-11-23 13:25:27 +0000

    created airport class with arraylist

---
+++ app/src/main/java/com/example/user/airport/Airport.java
@@ -0,0 +1,22 @@
+package com.example.user.airport;
+
+import java.util.ArrayList;
+
+/**
+ * Created by user on 23/11/2016.
+ */
+
+public class Airport {
+
+    protected ArrayList<Flight> flightList;
+
+    public Airport(){
+        this.flightList = new ArrayList<Flight>();
+    }
+
+
+
+
+
+
+}
---
+++ app/src/test/java/com/example/user/airport/AirportTest.java
@@ -0,0 +1,8 @@
+package com.example.user.airport;
+
+/**
+ * Created by user on 23/11/2016.
+ */
+
+public class AirportTest {
+}
