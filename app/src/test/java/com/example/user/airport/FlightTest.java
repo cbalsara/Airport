a4faf666457daf42f9549bb9363fefe84c2c8ad3
Cyrus <cdbalsara@icloud.com>
2016-11-23 13:31:29 +0000

    tested flight number

--- app/src/test/java/com/example/user/airport/FlightTest.java
+++ app/src/test/java/com/example/user/airport/FlightTest.java
@@ -1,8 +1,29 @@
 package com.example.user.airport;
 
+import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
 /**
  * Created by user on 23/11/2016.
  */
 
 public class FlightTest {
+
+    Flight flight;
+
+
+
+    @Before
+    public void Before(){
+        flight = new Flight(4365, "London");
+    }
+
+    @Test
+    public void TestFlightNumber(){
+        assertEquals( 4365, flight.getFlightNumber() );
+    }
+
+
 }
